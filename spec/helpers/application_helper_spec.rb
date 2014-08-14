9c69e77c883527cdd14a2a6b09f3e80621a2b96a
Ben Meier <benmeier42@gmail.com>
2014-08-14 20:55:31 +0200

    Add random passing rspec test to make sure its working

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe ApplicationHelper do
+
+    # just a junk test to make sure rspec is running nicely
+    it "verifies that true is true" do
+        expect(false).to eq(false)
+    end
+
+end

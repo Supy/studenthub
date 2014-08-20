075dfd56560897914db6dbf3befd4da19d311dc6
Ben Meier <benmeier42@gmail.com>
2014-08-20 14:11:13 +0200

    Added full CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
     get 'site/home'
 
-
-    get '/accomodation', to: 'accomodation#index'
-    get '/accomodation/new', to: 'accomodation#new'
-    get '/accomodation/edit/:id', to: 'accomodation#edit'
-    get '/accomodation/:id', to: 'accomodation#show'
+    # Accomodation can be handled as a CRUD resource:
+    # accomodation_index  GET    /accomodation(.:format)          accomodation#index
+    # accomodation_index  POST   /accomodation(.:format)          accomodation#create
+    # new_accomodation    GET    /accomodation/new(.:format)      accomodation#new
+    # edit_accomodation   GET    /accomodation/:id/edit(.:format) accomodation#edit
+    # accomodation        GET    /accomodation/:id(.:format)      accomodation#show
+    # accomodation        PATCH  /accomodation/:id(.:format)      accomodation#update
+    # accomodation        PUT    /accomodation/:id(.:format)      accomodation#update
+    # accomodation        DELETE /accomodation/:id(.:format)      accomodation#destroy
+    resources :accomodation
 
     root 'site#home'
 
--- spec/controllers/accomodation_controller_spec.rb
+++ spec/controllers/accomodation_controller_spec.rb
@@ -9,6 +9,13 @@ describe AccomodationController, :type => :controller do
         end
     end
 
+    describe 'POST index' do
+        it 'returns http success' do
+            post :index
+            expect(response).to be_success
+        end
+    end
+
     describe 'GET new' do
         it 'returns http success' do
             get :new
@@ -26,6 +33,27 @@ describe AccomodationController, :type => :controller do
     describe 'GET show' do
         it 'returns http success' do
             get :show, id: 1
+            expect(response).to be_success
+        end
+    end
+
+    describe 'PATCH update' do
+        it 'returns http success' do
+            patch :update, id: 1
+            expect(response).to be_success
+        end
+    end
+
+    describe 'PUT update' do
+        it 'returns http success' do
+            put :update, id: 1
+            expect(response).to be_success
+        end
+    end
+
+    describe 'DELETE destroy' do
+        it 'returns http success' do
+            delete :destroy, id: 1
             expect(response).to be_success
         end
     end

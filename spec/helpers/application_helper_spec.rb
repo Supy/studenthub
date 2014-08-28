367ee98dc8283ffa75f8b4b0e9afcebdeb42cfe2
Justin <contact@studenthub.co.za>
2014-08-28 17:09:09 +0200

    Removed junk from helper specs.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,9 +2,4 @@ require "rails_helper"
 
 describe ApplicationHelper do
 
-    # just a junk test to make sure rspec is running nicely
-    it "verifies that true is true" do
-        expect(false).to eq(false)
-    end
-
 end

19020a41c3044b10ff03bd21957a14b82c6482ed
Ben Meier <benmeier42@gmail.com>
2014-08-30 10:50:20 +0200

    removed useless variable

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,12 +4,10 @@ class LocationsController < ApplicationController
         root = Location.find_by(id: params[:id])
         children = root.nil? ? Location.roots : root.children
 
-        @result = {
+        render json: {
             root: root.nil? ? nil : root.simple,
             children: children.map { |c| c.simple  }
         }
-
-        render json: @result
     end
 
 end
